3a57af7e5cffd0e775e3f6e3a6b5710a0541df90
alekseyburn <alekseyburn@gmail.com>
2020-01-13 20:28:13 +0300

    добавил cssnano для copy:css, сделал запуск для разработки и продакшена, добавил watcher's для css и copy:css

--- gulpfile.js
+++ gulpfile.js
@@ -6,19 +6,39 @@ const sass = require('gulp-sass');  //компиляция scss
 const sourcemaps = require('gulp-sourcemaps');  //карты кода
 const postcss = require('gulp-postcss');  //преобразование и оптимизация css
 const cssnano = require('cssnano'); //минификация css
-const rename = require('gulp-rename'); //переименовывание файлов
+const rename = require('gulp-rename');  //переименовывание файлов
+const gulpIf = require('gulp-if');  //создание условий
 
+
+const isDev = !process.env.NODE_ENV || process.env.NODE_ENV === 'dev';
+
+gulp.task('copy:css', () => {
+  return gulp.src('src/css/*.css', {since: gulp.lastRun('copy:css')})
+    .pipe(postcss([
+      cssnano({
+        discardUnused: false  //не удалять неиспользуемые классы
+      })
+    ]))
+    .pipe(rename((path) => {
+      path.extname = '.min.css'
+    }))
+    .pipe(gulp.dest('build/css'));
+});
 
 gulp.task('css', () => {
   return gulp.src('src/sass/style.scss')
-    .pipe(sourcemaps.init())
+    .pipe(gulpIf(isDev,sourcemaps.init()))
     .pipe(sass())
     .pipe(postcss([
       cssnano(),
     ]))
     .pipe(rename('style.min.css'))
-    .pipe(sourcemaps.write('.'))
+    .pipe(gulpIf(isDev, sourcemaps.write('.')))
     .pipe(gulp.dest('build/css'));
 });
 
 gulp.task('clean:build', () => del('build'));
+gulp.task('build', gulp.series('clean:build', gulp.parallel('css', 'copy:css')));
+
+gulp.watch('src/sass/**/*.scss', gulp.series('scss'));
+gulp.watch('src/css/*.css', gulp.series('copy:css'));
