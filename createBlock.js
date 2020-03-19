ff3216068287abc5129e9e9dcf58c2e68f876149
alekseyburn <alekseyburn@gmail.com>
2020-03-19 16:31:32 +0300

    добавил svg-sprite,png-sprite таски, svg4everybody

--- createBlock.js
+++ createBlock.js
@@ -4,7 +4,7 @@
 
 const fs = require('fs'); // Для работы с файловой системой
 const pjson = require('./package.json'); // Чтобы получать настройки из package.json
-const dirs = pjson.config.directories; // Объект с директориями
+const dirs = pjson.configProject.dirs; // Объект с директориями
 const mkdirp = require('mkdirp'); // Зависимость - создание папки
 
 /*
@@ -15,67 +15,46 @@ process.argv[n]
 В нашем случае [2] - имя блока, [3-...] - имена расширений
  */
 let blockName = process.argv[2];
-let defaultExtensions = ['scss', 'html']; //расширения по умолчанию
+let defaultExtensions = ['scss', 'html', 'img']; //расширения по умолчанию
+
 //добавляем к массиву defaultExtensions additional extensions, заданные при вводе run node createBlock, затем удаляем дубликаты
 let extensions = uniqueArray(defaultExtensions.concat(process.argv.slice(3)));
 
 if (blockName) {
 
-  let dirPath = dirs.source + '/blocks/' + blockName + '/';
+  let dirPath = dirs.srcPath + dirs.blocksDirName + '/' + blockName + '/';
   mkdirp(dirPath).then(() => {
     console.log('[NTH] Создание папки ' + dirPath + ' (создана, если ещё не существует)');
-
-    // Читаем файл диспетчера подключений
-    let connectManager = fs.readFileSync(dirs.source + '/sass/style.scss', 'utf8');
-
-    // Делаем из строк массив, фильтруем, оставляя только строки с незакомментированными импортами
-    let fileSystem = connectManager.split('\n').filter(item => {
-      return /^(\s*)@import/.test(item);
-    });
 
     extensions.forEach((extension) => {
       let filePath = dirPath + blockName + '.' + extension;
       let fileContent = '';
-      let SCSSfileImport = ''; // конструкция импорта будущего scss
       let fileCreateMsg = '';
       if (extension === 'scss') {
-        SCSSfileImport = '@import \'' + dirs.source + '/blocks/' + blockName + '/' + blockName + '.scss\';';
-        fileContent = '.' + blockName + ' {\n  \n}\n';
+        fileContent = '// В этом файле должны быть стили только для БЭМ-блока ' + blockName + ', его элементов, \n//модификаторов, псевдоселекторов, псевдоэлементов, @media-условий...\n// Не пишите здесь другие селекторы.\n\n.' + blockName + ' {\n  \n}\n';
 
-        let reg = new RegExp(SCSSfileImport, '');
-
-        // флаг отсутствия блока среди импортов
-        let importExist = false;
-
-        // Обойдем массив и проверим наличие импорта
-        for (let item of fileSystem) {
-          if (reg.test(item)) {
-            importExist = true;
+        // Добавим созданный файл в ./package.json
+        let hasThisBlock = false;
+        for (let block in pjson.configProject.blocks) {
+          if (block === blockName) {
+            hasThisBlock = true;
             break;
           }
         }
 
-        // Если импорт по-прежнему false, то допишем импорт
-        if (!importExist) {
-          fs.open(dirs.source + '/sass/style.scss', 'a', (err, fileHandle) => {
-            if (!err) {
-              fs.write(fileHandle, SCSSfileImport + '\n', null, 'utf8', (err, written) => {
-                if (!err) {
-                  console.log('[NTH] В диспетчер подключений (' + dirs.source + '/sass/style.scss) записано: ' + SCSSfileImport);
-                } else {
-                  console.log('[NTH] ОШИБКА записи в ' + dirs.source + '/sass/style.scss: ' + err);
-                }
-              });
-            } else {
-              console.log('[NTH] ОШИБКА открытия ' + dirs.source + '/sass/style.scss: ' + err);
-            }
-          });
-        } else {
-          console.log('[NTH] Импорт НЕ прописан в ' + dirs.source + '/sass/style.scss (он там уже есть)');
+        if (!hasThisBlock) {
+          pjson.configProject.blocks[blockName] = [];
+          let newPackageJson = JSON.stringify(pjson, '', 2);
+          fs.writeFileSync('./package.json', newPackageJson);
+          fileCreateMsg = '[NTH] Подключение блока добавлено в package.json';
         }
-      } else if (extension === 'html') {
+      }
+
+      else if (extension === 'html') {
         fileContent = '<div class="' + blockName + '">content</div>\n';
-      } else if (extension === 'js') {
+      }
+
+      else if (extension === 'js') {
         fileContent = '// (function(){\n// код\n// }());\n';
       }
 
@@ -84,8 +63,8 @@ if (blockName) {
         let imgFolder = dirPath + 'img/';
         if (fileExist(imgFolder) === false) {
           mkdirp(imgFolder).then(() => {
-            console.log('[NTH] Папка создана: ' + imgFolder);
-          })
+            console.log('[NTH] Папка создана: ' + imgFolder + ' (если отсутствует)');
+          });
         }
       }
 
